55a24ad9843f0ca3fcbf2d0d836ba6ab90844d20
Sanij Shrestha <saniz.sth123@gmail.com>
2023-09-24 23:42:17 -0400

    [added] date Dialog

--- app/src/main/java/eu/tutorials/ageinminutes/MainActivity.kt
+++ app/src/main/java/eu/tutorials/ageinminutes/MainActivity.kt
@@ -11,8 +11,38 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private var tvSelectedDate: TextView?  = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvSelectedDate = findViewById(R.id.showDate)
+        val btnDatePicker : Button = findViewById(R.id.btnDatePicker) // connecting the variable to a specific id.
+        btnDatePicker.setOnClickListener {
+            clickDatePicker()
+
+        }
+
+    }
+    fun clickDatePicker() {
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        val selectedDate =
+        DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener{view, selectedYear, selectedMonth, selectedDayOfMonth ->
+                Toast.makeText(this, "year was $selectedYear, month was $selectedMonth, day was $selectedDayOfMonth", Toast.LENGTH_LONG).show()
+
+                val selectedDate = "$selectedYear/${selectedMonth + 1}/$selectedDayOfMonth"
+                tvSelectedDate?.setText(selectedDate)
+            },
+            year,
+            month,
+            day
+            ).show()
+
+
     }
 }
